303d97e7ce9ee11584f50b481c3443e1d8910507
pcego <pcego36@gmail.com>
2014-05-01 17:47:29 -0300

    Tratamento Exceções

--- drugstore/src/br/com/kpc/drugstore/dao/DaoClient.java
+++ drugstore/src/br/com/kpc/drugstore/dao/DaoClient.java
@@ -9,6 +9,7 @@ import br.com.kpc.drugstore.core.IRepositoryClient;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Query;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,25 +19,41 @@ public class DaoClient extends DaoGeneric<Client> implements IRepositoryClient {
 
     public DaoClient() {
         super(Client.class);
+
     }
 
     @Override
     public List<Client> listAll(boolean active) {
 
-        Query query = getManager().createQuery("SELECT C FROM Client C WHERE "
-                + "C.active = :active ORDER BY C.name");
-        query.setParameter("active", active);
-        return query.getResultList();
+        try {
+            Query query = getManager().createQuery("SELECT C FROM Client C WHERE "
+                    + "C.active = :active ORDER BY C.name");
+            query.setParameter("active", active);
+            return query.getResultList();
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Falha ao Listar Clientes",
+                    "Ihh Falhou...!!", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+        }
+
     }
 
     @Override
     public Client getClientByCpf(String cpf, boolean active) {
 
-        Query query = getManager().createQuery("SELECT C FROM Client C WHERE "
-                + "C.cpf = :cpf AND C.active = :active ORDER BY C.name");
-        query.setParameter("cpf", cpf);
-        query.setParameter("active", active);
-        return (Client) query.getSingleResult();
+        try {
+            Query query = getManager().createQuery("SELECT C FROM Client C WHERE "
+                    + "C.cpf = :cpf AND C.active = :active ORDER BY C.name");
+            query.setParameter("cpf", cpf);
+            query.setParameter("active", active);
+            return (Client) query.getSingleResult();
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Falha ao Carregar Cliente",
+                    "Ihh Falhou...!!", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+        }
     }
 
     @Override
@@ -46,37 +63,64 @@ public class DaoClient extends DaoGeneric<Client> implements IRepositoryClient {
                 + "WHERE MONTH(C.DT_NASC) = MONTH(CURDATE()) "
                 + "AND DAY(C.DT_NASC) = DAY(CURDATE()) ORDER BY C.NAME;";
 
-        Query query = getManager().createNativeQuery(stQuery, Client.class);        
-        return query.getResultList();
+        try {
+            Query query = getManager().createNativeQuery(stQuery, Client.class);
+            return query.getResultList();
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Falha ao Listar Clientes",
+                    "Ihh Falhou...!!", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+        }
     }
 
     @Override
     public List<Client> listClientByName(String name, boolean active) {
 
-        Query query = getManager().createQuery("SELECT C FROM Client C WHERE"
-                + " C.name LIKE :name AND C.active = :active ORDER BY C.name");
-        query.setParameter("name", name);
-        query.setParameter("active", active);
-        return query.getResultList();
+        try {
+            Query query = getManager().createQuery("SELECT C FROM Client C WHERE"
+                    + " C.name LIKE :name AND C.active = :active ORDER BY C.name");
+            query.setParameter("name", name);
+            query.setParameter("active", active);
+            return query.getResultList();
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Falha ao Listar Clientes",
+                    "Ihh Falhou...!!", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+        }
     }
 
     @Override
     public List<Client> listClientByCity(String city, boolean active) {
 
-        Query query = getManager().createQuery("SELECT C FROM Client C WHERE C.adress_city LIKE :city "
-                + "AND C.active = :active GROUP BY C.adress_city ORDER BY C.name");
-        query.setParameter("city", city);
-        query.setParameter("active", active);
-        return query.getResultList();
+        try {
+            Query query = getManager().createQuery("SELECT C FROM Client C WHERE C.adress_city LIKE :city "
+                    + "AND C.active = :active GROUP BY C.adress_city ORDER BY C.name");
+            query.setParameter("city", city);
+            query.setParameter("active", active);
+            return query.getResultList();
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Falha ao Listar Clientes",
+                    "Ihh Falhou...!!", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+
+        }
     }
 
     @Override
     public int clientRecipeValid(Date lastRecipe) {
 
-        final String strQuery = "SELECT DATEDIFF(CURDATE(),"+lastRecipe+");"; 
-        Query query = getManager().createNativeQuery(strQuery);
-        return query.getFirstResult();
+        final String strQuery = "SELECT DATEDIFF(CURDATE()," + lastRecipe + ");";
 
+        try {
+            Query query = getManager().createNativeQuery(strQuery);
+            return query.getFirstResult();
+        } catch (Exception ex) {
+
+            return -1;
+        }
     }
 
 }
