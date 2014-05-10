72778edb011e5676ecd678037f2c64f7e1ce3816
pcego <pcego36@gmail.com>
2014-05-10 11:32:36 -0300

    criando classe e método para instancia de janelas

---
+++ drugstore/src/br/com/kpc/drugstore/service/WindowInstance.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.drugstore.service;
+
+import br.com.kpc.drugstore.gui.ClienteFrame;
+import br.com.kpc.drugstore.gui.CompanyFrame;
+import br.com.kpc.drugstore.gui.ConsultarCliente;
+import br.com.kpc.drugstore.gui.RecipeFrame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Pcego
+ */
+public class WindowInstance {
+
+    private static ClienteFrame client;
+    private static CompanyFrame company;
+    private static RecipeFrame recipe;
+    private static ConsultarCliente search;
+
+    public static final int CLIENTWINDOW = 1;
+    public static final int COMPANYWINDOW = 2;
+    public static final int RECIPEWINDOW = 3;
+    public static final int SEARCHWINDOW = 4;
+
+    public static JFrame getInstance(int id) {
+
+        switch (id) {
+
+            case CLIENTWINDOW:
+
+                if (client == null) {
+                    client = new ClienteFrame();
+                }
+                return client;
+
+            case COMPANYWINDOW:
+
+                if (company == null) {
+                    company = new CompanyFrame();
+                }
+                return company;
+
+            case RECIPEWINDOW:
+
+                if (recipe == null) {
+                    recipe = new RecipeFrame();
+                }
+                return recipe;
+
+            case SEARCHWINDOW:
+
+                if (search == null) {
+                    search = new ConsultarCliente();
+                }
+                return search;
+
+            default:
+                return null;
+        }
+
+    }
+
+}
