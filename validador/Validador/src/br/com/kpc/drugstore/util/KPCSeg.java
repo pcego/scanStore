1b81b9c84afe7d0d05fd4ee3f5ee7e000104902a
Kennedi Paulo <kennedimalheiros@gmail.com>
2014-06-13 21:42:51 -0300

    Commit Inicial

---
+++ validador/Validador/src/br/com/kpc/drugstore/util/KPCSeg.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.drugstore.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ *
+ * @author www
+ */
+public class KPCSeg {
+
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private KeySpec ks;
+    private SecretKeyFactory skf;
+    private Cipher cipher;
+    byte[] arrayBytes;
+    private String myEncryptionKey;
+    private String myEncryptionScheme;
+    SecretKey key;
+
+    public KPCSeg() throws Exception {
+        myEncryptionKey = "KopsJHsIKjhkhas654asd564";
+        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+
+    public String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+    public String decrypt(String encryptedString) {
+        String decryptedText = null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decodeBase64(encryptedString.getBytes());
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText = new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+
+    public static void main(String args[]) throws Exception {
+        KPCSeg td = new KPCSeg();
+
+        String target = "imparator";
+        String encrypted = td.encrypt(target);
+        String decrypted = td.decrypt(encrypted);
+
+        System.out.println("String To Encrypt: " + target);
+        System.out.println("Encrypted String:" + encrypted);
+        System.out.println("Decrypted String:" + decrypted);
+
+    }
+
+    // INICIO BUSCAR INFORMAÇÕES DO COMPUTADOR
+    public static String getHDSerial(String drive) {
+        String result = "";
+        try {
+            //File file = File.createTempFile("tmp",".vbs");   
+            File file = File.createTempFile("tmp", ".vbs");
+            file.deleteOnExit();
+            FileWriter fw = new java.io.FileWriter(file);
+
+            String vbs = "Set objFSO = CreateObject(\"Scripting.FileSystemObject\")\n" + "Set colDrives = objFSO.Drives\n"
+                    + "Set objDrive = colDrives.item(\"" + drive + "\")\n" + "Wscript.Echo objDrive.SerialNumber";
+            fw.write(vbs);
+            fw.close();
+            Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+            BufferedReader input
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                result += line;
+            }
+            input.close();
+        } catch (Exception e) {
+
+        }
+        if (result.trim().length() < 1 || result == null) {
+            result = "NO_DISK_ID";
+
+        }
+
+        return result.trim();
+    }
+
+    public static String getCPUSerial() {
+        String result = "";
+        try {
+            File file = File.createTempFile("tmp", ".vbs");
+            file.deleteOnExit();
+            FileWriter fw = new java.io.FileWriter(file);
+
+            String vbs
+                    = "On Error Resume Next \r\n\r\n"
+                    + "strComputer = \".\"  \r\n"
+                    + "Set objWMIService = GetObject(\"winmgmts:\" _ \r\n"
+                    + "    & \"{impersonationLevel=impersonate}!\\\\\" & strComputer & \"\\root\\cimv2\") \r\n"
+                    + "Set colItems = objWMIService.ExecQuery(\"Select * from Win32_Processor\")  \r\n "
+                    + "For Each objItem in colItems\r\n "
+                    + "    Wscript.Echo objItem.ProcessorId  \r\n "
+                    + "    exit for  ' do the first cpu only! \r\n"
+                    + "Next                    ";
+
+            fw.write(vbs);
+            fw.close();
+            Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+            BufferedReader input
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                result += line;
+            }
+            input.close();
+        } catch (Exception e) {
+
+        }
+        if (result.trim().length() < 1 || result == null) {
+            result = "NO_CPU_ID";
+        }
+        return result.trim();
+    }
+    // FIM - BUSCAR INFORMAÇÕES DO COMPUTADOR
+
+}
