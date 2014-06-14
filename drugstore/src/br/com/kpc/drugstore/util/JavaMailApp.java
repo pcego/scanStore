13c382c04e481b525c09ed67852959bed7d0f9ee
Kennedi Paulo <kennedimalheiros@gmail.com>
2014-06-14 18:12:28 -0300

    Envio de Email

---
+++ drugstore/src/br/com/kpc/drugstore/util/JavaMailApp.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.drugstore.util;
+
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author www
+ */
+public class JavaMailApp {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        /**
+         * Parâmetros de conexão com servidor Gmail
+         */
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("kpcvalidacao@gmail.com", "PC//kpsm2013");
+                    }
+                });
+
+        /**
+         * Ativa Debug para sessão
+         */
+        session.setDebug(true);
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("kpcvalidacao@gmail.com")); //Remetente
+
+            Address[] toUser = InternetAddress //Destinatário(s)
+                    .parse("mobilekpc@gmail.com, pcego36@gmail.com , kennedimalheiros@gmail.com");
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject("Solicitação de Validação scanStore");//Assunto
+            message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
+            /**
+             * Método para enviar a mensagem criada
+             */
+            Transport.send(message);
+
+            System.out.println("Feito!!!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
