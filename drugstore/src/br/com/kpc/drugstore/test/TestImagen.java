9853bbd78e4b45147346256767b5fcb27e459806
pcego <pcego36@gmail.com>
2014-06-15 13:54:07 -0300

    implementando teste de relatórios

--- drugstore/src/br/com/kpc/drugstore/test/TestImagen.java
+++ drugstore/src/br/com/kpc/drugstore/test/TestImagen.java
@@ -47,6 +47,7 @@ public class TestImagen extends javax.swing.JFrame {
         imagen = new javax.swing.JLabel();
         carregarImg = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -64,6 +65,13 @@ public class TestImagen extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Documentos");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -74,7 +82,9 @@ public class TestImagen extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(carregarImg)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1))
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton2))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(127, 127, 127)
                         .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -88,7 +98,8 @@ public class TestImagen extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(carregarImg)
-                    .addComponent(jButton1))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addGap(21, 21, 21))
         );
 
@@ -98,7 +109,7 @@ public class TestImagen extends javax.swing.JFrame {
     private void carregarImgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_carregarImgActionPerformed
 
         File img = new File("C:\\imagens\\testando.jpg");
-        
+
         try {
             BufferedImage bfi = ImageIO.read(img);
             imagen.setIcon(new ImageIcon(bfi));
@@ -112,12 +123,12 @@ public class TestImagen extends javax.swing.JFrame {
         HashMap hm = new HashMap();
         hm.put("active_opt", true);
         File rel = new File("C:\\scanStore\\drugstore\\src\\br\\com\\kpc\\drugstore\\relatorios\\RelClientsByStatus.jrxml");
-              
+
         String data = null;
-        
+
         new SimpleDateFormat("yyyy/MM/dd").format(data);
         System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(data));
-        
+
         try {
             //gerando o jasper design
             JasperDesign desenho = JRXmlLoader.load(rel);
@@ -130,6 +141,35 @@ public class TestImagen extends javax.swing.JFrame {
             System.out.println("deu erro: " + jrex);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        HashMap hm2 = new HashMap();
+        StringBuilder sql_base = new StringBuilder("select cl.name, cl.cpf, cl.cpf_image, cl.rg_image, r.dt_recipe, r.recipe_type, \n" +
+"r.recipe_image, r.other_document, t.dt_shop, t.ticket_image, \n" +
+"t.ticket_number, t.auth_number from clients as cl INNER join recipes as r\n" +
+"on cl.clientId = r.CLIENT_clientId INNER join tickets as t\n" +
+"on r.recipeId = t.RECIPE_recipeId ");
+        
+        String filtro = "WHERE cl.CPF = 95111212604 and "
+                + "r.recipeId = (select MAX(recipeId) from recipes where clientId = (select clientId from clients where cpf = 95111212604))";
+       // sql_base.append(filtro);
+        hm2.put("query", sql_base);
+       
+        File rel = new File("C:\\scanStore\\drugstore\\src\\br\\com\\kpc\\drugstore\\relatorios\\documentos.jrxml");
+
+        try {
+            //gerando o jasper design
+            JasperDesign desenho = JRXmlLoader.load(rel);
+            //compila o relatório
+            JasperReport relatorio = JasperCompileManager.compileReport(desenho);
+            JasperPrint print = JasperFillManager.fillReport(relatorio, hm2, ConnectionDb.getConnectionDs());
+            JasperViewer visao = new JasperViewer(print, true);
+            visao.setVisible(true);
+        } catch (JRException jrex) {
+            System.out.println("deu erro: " + jrex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -170,5 +210,6 @@ public class TestImagen extends javax.swing.JFrame {
     private javax.swing.JButton carregarImg;
     private javax.swing.JLabel imagen;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     // End of variables declaration//GEN-END:variables
 }
