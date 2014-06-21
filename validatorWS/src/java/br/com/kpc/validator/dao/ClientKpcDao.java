3919515cf0b9a9dfe126bdf73c5f43889ad48975
pcego <pcego36@gmail.com>
2014-06-20 21:42:14 -0300

    implementando métodos da interface

--- validatorWS/src/java/br/com/kpc/validator/dao/ClientKpcDao.java
+++ validatorWS/src/java/br/com/kpc/validator/dao/ClientKpcDao.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.kpc.validator.dao;
 
 import br.com.kpc.validator.core.ClientKpc;
 import br.com.kpc.validator.core.IRepositoryClientKpc;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 /**
  *
@@ -22,15 +22,33 @@ public class ClientKpcDao extends GenericDao<ClientKpc> implements IRepositoryCl
         super(ClientKpc.class);
     }
 
-    
     @Override
     public List<ClientKpc> listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+
+            Query query = getManager().createQuery("select cl from ClientKpc cl order by cl.name");
+            return query.getResultList();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public ClientKpc getClient(String cpfCnpj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+
+            Query query = getManager().createQuery("select cl from ClientKpc cl where cl.cpf_Cnpj = :cpf_Cnpj");
+            query.setParameter("cpf_Cnpj", cpfCnpj);
+            return (ClientKpc) query.getSingleResult();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
-    
+
 }
