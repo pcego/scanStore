74ce75edf0327f6dddcd756f6f543de78c0e3114
Kennedi Paulo <kennedimalheiros@gmail.com>
2014-06-24 23:13:21 -0300

    Add classe converter date

---
+++ validatorWS/src/java/br/com/kpc/validator/resource/FormatDate.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.validator.resource;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Pcego
+ */
+public class FormatDate {
+
+    public static String dateCovertToShow(Date date) {
+
+        return new SimpleDateFormat("dd/MM/yyyy").format(date);
+    }
+
+    public static Date transformaData(String data) {
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return formatador.parse(data);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
