f9da0164c324831c0f048564f5987cae2dcb150f
kennedimalheiros <kennedimalheiros@gmail.com>
2014-08-06 00:06:47 -0300

    Criando metodo para verificar diferenças entre datas

--- drugstore/src/br/com/kpc/drugstore/util/FormatDate.java
+++ drugstore/src/br/com/kpc/drugstore/util/FormatDate.java
@@ -5,9 +5,17 @@
  */
 package br.com.kpc.drugstore.util;
 
+import br.com.kpc.drugstore.gui.ClienteFrame;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  *
@@ -29,4 +37,23 @@ public class FormatDate {
         }
     }
 
+    public static long diferencaDias(String dtInicial, String dtFinal) {
+        
+        GregorianCalendar ini = new GregorianCalendar();
+        GregorianCalendar fim = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            ini.setTime(sdf.parse(dtInicial));
+            fim.setTime(sdf.parse(dtFinal));
+        } catch (Exception e) {
+        }
+
+        long dt1 = ini.getTimeInMillis();
+        long dt2 = fim.getTimeInMillis();
+
+        System.out.println("Total " +  ((dt2 - dt1) / 86400000) );
+
+        return ((dt2 - dt1) / 86400000);
+    }
 }
