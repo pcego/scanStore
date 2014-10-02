03cf27bc386c067ab1c8d02c18e42ac9831fcb3c
pcego <pcego36@gmail.com>
2014-10-02 00:37:28 -0300

    criando filtros

--- drugstore/src/br/com/kpc/drugstore/relatorios/Filter.java
+++ drugstore/src/br/com/kpc/drugstore/relatorios/Filter.java
@@ -5,34 +5,27 @@
  */
 package br.com.kpc.drugstore.relatorios;
 
-import br.com.kpc.drugstore.dao.ConnectionDb;
-import java.io.File;
-import java.util.Date;
-import java.util.HashMap;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
-import net.sf.jasperreports.view.JasperViewer;
-
 /**
  *
  * @author pcego
  */
 public class Filter {
 
-    //passar null como parametro se não existir
-    public String createSqlRel(String cpf, Date shopIn, Date shopEnd, Date recipeIn, Date recipeEnd) {
+    public static final int DATA_RECEITA_CLIENTE = 1;
+    public static final int INTERVALO_DATA_RECEITA = 2;
+    public static final int DATA_VENDA = 3;
+    public static final int INTERVALO_DATA_VENDA = 4;
+    public static final int TODOS = 5;
+    public static final int ULTIMA_COMPRA_CLIENTE = 6;
+    public static final int VENDAS_CLIENTE = 7;
+    public static final int CLIENTE_DATA_VENDA = 8;
+    public static final int DATA_RECEITA = 9;
 
-        HashMap hm = new HashMap();
-        
-        if(cpf.equals(null)){
-            
-        }      
-        
+    //passar null como parametro se não existir
+    public StringBuilder createSqlRel(int check, String cpf, String dataIn, String dataEnd) {
+
+        String filtro = "";
+
         //sql base para geração de todos os relatórios para listagem de documentos
         //sem o filtro essa sql lista todas as vendas contidas na base
         StringBuilder sql_base = new StringBuilder("select cl.name, cl.cpf, cl.cpf_image, cl.rg_image, r.dt_recipe, r.recipe_type, \n"
@@ -40,40 +33,45 @@ public class Filter {
                 + "t.ticket_number, t.auth_number from clients as cl INNER join recipes as r\n"
                 + "on cl.clientId = r.CLIENT_clientId INNER join tickets as t\n"
                 + "on r.recipeId = t.RECIPE_recipeId ");
-        
-        
-        //implementação dos filtros para relatórios de documentos
-        //estes são apenas alguns filtros possíveis sendo que novos filtros podem ser
-        //implementados assim que surgir a necessidade, utilizando-se sempre a mesma sql base
-        //filtra ultima compra de um cpf específico
-        String filtroUltimaCompra = "WHERE cl.CPF = 95111212604 and "
-                + "r.recipeId = (select MAX(recipeId) from recipes where CLIENT_clientId = (select clientId from clients where cpf = 95111212604))";
 
-        //filtra as vendas em uma determinada data
-        String filtroPorData = "WHERE t.DT_SHOP = '2014-08-01'";
+        switch (check) {
 
-        //filtra vendas para uma cliente específico em uma data específica
-        String filtroVendaClienteData = "WHERE cl.cpf = '95111212604' and t.DT_SHOP = '2014-08-01'";
+            case TODOS:
+                return sql_base;
 
-        //filtra todas as vendas com data de receita específica
-        String filtroVendaDataReceita = "WHERE r.DT_RECIPE = '2014-07-30'";
+            case VENDAS_CLIENTE:
+                filtro = " where cl.CPF = " + cpf;
+                return sql_base.append(filtro);
 
-        sql_base.append(filtroUltimaCompra);
+            case ULTIMA_COMPRA_CLIENTE:
+                filtro = "WHERE cl.CPF = " + cpf + " and "
+                        + "r.recipeId = (select MAX(recipeId) from recipes where "
+                        + "CLIENT_clientId = (select clientId from clients where cpf = " + cpf + " ))";
+                return sql_base.append(filtro);
 
-        hm.put("query", sql_base);
+            case INTERVALO_DATA_VENDA:
+                filtro = "WHERE t.DT_SHOP = BETWEEN " + dataIn + " AND " + dataEnd;
+                return sql_base.append(filtro);
+                
+            case INTERVALO_DATA_RECEITA:
+                filtro = "WHERE t.DT_RECIPE = BETWEEN " + dataIn + " AND " + dataEnd;
+                return sql_base.append(filtro);
+                
+            case DATA_VENDA:
+                filtro = "WHERE t.DT_SHOP = " + dataIn;
+                return sql_base.append(filtro);
 
-        File rel = new File("C:\\scanStore\\drugstore\\src\\br\\com\\kpc\\drugstore\\relatorios\\documentos.jrxml");
+            case DATA_RECEITA:
+                filtro = "WHERE r.DT_RECIPE = " + dataIn;
+                return sql_base.append(filtro);
 
-        try {
-            //gerando o jasper design
-            JasperDesign desenho = JRXmlLoader.load(rel);
-            //compila o relatório
-            JasperReport relatorio = JasperCompileManager.compileReport(desenho);
-            JasperPrint print = JasperFillManager.fillReport(relatorio, hm, ConnectionDb.getConnectionDs());
-            JasperViewer visao = new JasperViewer(print, true);
-            visao.setVisible(true);
-        } catch (JRException jrex) {
-            System.out.println("deu erro: " + jrex);
+            case CLIENTE_DATA_VENDA:
+                filtro = "WHERE cl.cpf = " + cpf + " and  t.DT_SHOP = " + dataIn;
+                return sql_base.append(filtro);
+
+            case DATA_RECEITA_CLIENTE:
+                filtro = "WHERE cl.cpf = " + cpf + " and  t.DT_RECIPE = " + dataIn;
+                return sql_base.append(filtro);
         }
 
         return null;
